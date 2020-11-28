83cefbdd4459e44148801fbae59bdf9cf1c06133
cjpmaubry <cjpm.aubry@gmail.com>
2020-11-28 14:44:51 +0100

    Adding routes and mongo import

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-
+from pymongo import MongoClient
 from flask import Flask, render_template
 
 
@@ -8,6 +8,18 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
+@app.route('/adminView')
+def adminView():
+    return render_template('adminView.html')
+    
+@app.route('/analystView')
+def analystView():
+    return render_template('analystView.html')
+
+@app.route('/userView')
+def userView():
+    return render_template('userView.html')
 
 if __name__ == "__main__":
     app.run()

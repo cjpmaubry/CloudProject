36f542d72606ed6af1dca45970bbe14146eacdf0
cjpmaubry <cjpm.aubry@gmail.com>
2020-12-03 19:46:59 +0100

    Added parameters for query

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 import pymongo #pip install pymongo
 import time  
-from flask import Flask, render_template # pip install flask
+from flask import Flask, render_template, request # pip install flask
 from sshtunnel import SSHTunnelForwarder # pip install sshtunnel 
 
 
@@ -19,11 +19,11 @@ server = SSHTunnelForwarder(
     remote_bind_address=(MONGO_DB, 22)
 )
 
-def launchQuery(resquestNumber):
+def launchQuery(resquestNumber,parametre):
     server.start()
     client = pymongo.MongoClient(MONGO_HOST, 30000)
     db = client[MONGO_DB]
-    data = executeQueryNb(db,resquestNumber)
+    data = executeQueryNb(db,resquestNumber,parametre)
     server.stop()
     return data
 
@@ -42,16 +42,16 @@ class Timer(object):
 
 app = Flask(__name__)
 
-def executeQueryNb(db,number):
+def executeQueryNb(db,number,parametre):
     data =''
     if number == 1 :
-        data = db.users.find({"Id": 7}, {"PostIds": 1,"CommentId": 1})
+        data = db.users.find({"Id":int(parametre)}, {"PostIds": 1,"CommentId": 1})
     if number == 2 :
-        data =  db.users.find({"PostIds": {"$in": [X] }}, {"Badges": 1})
+        data =  db.users.find({"PostIds": {"$in": int(parametre) }}, {"Badges": 1})
     if number == 3 :
-        data =  db.posts.find({"Title": {"$regex":"X"}}, {"_id":0,"Title": 1}).sort({"CommentCount": -1})
+        data =  db.posts.find({"Title": {"$regex":parametre}}, {"_id":0,"Title": 1}).sort({"CommentCount": -1})
     if number == 4 :
-        postUsers = db.Users.find({"Id": X}, {"PostIds": 1,"CommentId.PostId": 1})
+        postUsers = db.Users.find({"Id": int(parametre)}, {"PostIds": 1,"CommentId.PostId": 1})
         C = postUsers.toArray()
         Tab = []
         C_len = len(C[0]["PostIds"])
@@ -72,7 +72,7 @@ def executeQueryNb(db,number):
     if number == 8 :
         db.UsersAvg.drop()
         B = []
-        _posts = db.posts.find({"Tags":"prior"}, {"Comments.Id":1,"_id":0})
+        _posts = db.posts.find({"Tags":parametre}, {"Comments.Id":1,"_id":0})
         for i in range(len(_posts)):
             B.push(_posts[i]["Comments"])
         #.forEach(function(Comments){B.push(Comments)})
@@ -105,81 +105,94 @@ def userView():
     return render_template('userView.html')
 
 
-@app.route('/req1')
+@app.route('/req1',methods = ['POST'])
 def req1():
-    query_description =""
+    query_description ='L historique des commentaires et des posts pour un utilisateur donné'
+    result = request.form
+    id = result['id_user']
     timer = Timer()  
     timer.start()
-    data= launchQuery(1)
-    timer.stop()  
+    data= launchQuery(1,id)
+    timer.stop()
     return render_template('req.html',data = data, time = timer.interval, query_decription= query_description)
 
 
-@app.route('/req2')
+@app.route('/req2',methods = ['POST'])
 def req2():
-    query_description =""
+    query_description ="Les informations relatives aux badges de l'utilisateur d'un post donné"
+    result = request.form
+    id = result['id_post']
     timer = Timer()  
     timer.start()
-    data= launchQuery(2)
+    data= launchQuery(2,id)
     timer.stop()  
     return render_template('req.html',data = data, time = timer.interval, query_decription= query_description)
 
 
 
-@app.route('/req3')
+@app.route('/req3',methods = ['POST'])
 def req3():
-    query_description =""
+    query_description =" La liste des posts dont le titre ou le corps contient un mot clef donné, trié par le nombre de commentaires associés.  "
+    result = request.form
+    mot_clef = result['mot_clef']
     timer = Timer()  
     timer.start()
-    data= launchQuery(3)
+    data= launchQuery(3,mot_clef)
     timer.stop()  
     return render_template('req.html',data = data, time = timer.interval, query_decription= query_description)
 
 
 
-@app.route('/req4')
+@app.route('/req4',methods = ['POST'])
 def req4():
-    query_description =""
+    query_description ="La liste des posts actifs pour un utilisateur, trié par le score du post (c’est-à-dire les posts qu'il a créé ou commenté et qui n’ont pas encore été fermés.) "
+    result = request.form
+    id = result['id_user']
     timer = Timer()  
     timer.start()
-    data= launchQuery(4)
+    data= launchQuery(4,id)
     timer.stop()  
     return render_template('req.html',data = data, time = timer.interval, query_decription= query_description)
 
-@app.route('/req5')
+@app.route('/req5',methods = ['POST'])
 def req5():
-    query_description =""
+    query_description ="Le post ayant la durée d'activité la plus longue par tag (pour le tag 1 c’est le post Y ; pour le tag 2 c’est le post X etc…)  "
+    noparam=""
     timer = Timer()  
     timer.start()
-    data= launchQuery(5)
+    data= launchQuery(5,noparam)
     timer.stop()  
     return render_template('req.html',data = data, time = timer.interval, query_decription= query_description)
 
-@app.route('/req6')
+@app.route('/req6',methods = ['POST'])
 def req6():
-    query_description =""
+    query_description ="L'utilisateur le plus actif en prenant en considération le nombre de votes positifs et le nombre de commentaires  "
+    noparam=""
     timer = Timer()  
     timer.start()
-    data= launchQuery(6)
+    data= launchQuery(6,noparam)
     timer.stop()  
     return render_template('req.html',data = data, time = timer.interval, query_decription= query_description)
 
 
-@app.route('/req7')
+@app.route('/req7',methods = ['POST'])
 def req7():
-    query_description =""
+    query_description ="Le pourcentage d’obtention de chaque badge"
+    noparam=""
     timer = Timer()  
     timer.start()
-    data= launchQuery(7)
+    data= launchQuery(7,noparam)
     timer.stop()  
     return render_template('req.html',data = data, time = timer.interval, query_decription= query_description)
 
-@app.route('/req8')
+@app.route('/req8',methods = ['POST'])
 def req8():
-    query_description =""
+    query_description ="L’âge moyen des utilisateurs commentant les posts d’un certain tag"
+    result = request.form
+    tag = result['tag']
     timer = Timer()  
     timer.start()
-    data= launchQuery(8)
+    data= launchQuery(8,tag)
     timer.stop()  
     return render_template('req.html',data = data, time = timer.interval, query_decription= query_description)
 
